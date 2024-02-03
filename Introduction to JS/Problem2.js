aece4eaa64d4eb4b38aa736207071b856cc6f3dc
Sourav <souravpramanick1997@gmail.com>
2024-02-04 03:38:44 +0530

    Pushing Introduction to JS

---
+++ Introduction to JS/Problem2.js
@@ -0,0 +1,6 @@
+var _name = "Sourav"
+console.log(_name)
+_name = "Sunirmal"
+console.log(_name)
+_name = "Mousumi"
+console.log(_name)
---
+++ Introduction to JS/Problem3.js
@@ -0,0 +1,4 @@
+var _name ="Sourav"
+var age = 21
+console.log(_name,age)
+console.log(typeof(_name),typeof(age))
